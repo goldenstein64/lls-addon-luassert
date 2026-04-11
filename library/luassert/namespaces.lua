41beb724bdb8acd88f55d0a53a7080379a2098a9
goldenstein64 <goldnstein64@gmail.com>
2026-04-11 02:19:28 -0400

    feat: add namespaces

---
+++ library/luassert/namespaces.lua
@@ -0,0 +1,8 @@
+---@class luassert.namespaces
+---@field assertion { [string]: any }
+---@field modifier { [string]: any }
+---@field matcher { [string]: any }
+---@field [string] { [string]: any }
+local namespaces = nil
+
+return namespaces
